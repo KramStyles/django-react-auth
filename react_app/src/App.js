bd0db971fa12804d9e7ca752debaf6cfc149118a
kramstyles <mark.eke@decagon.dev>
2022-05-05 22:25:57 +0100

    added register js

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 
 import LoginForm from "./components/LoginForm";
+import {RegisterForm} from "./components/RegisterForm";
 
 function App() {
   return (
-    <LoginForm/>
+    <RegisterForm/>
   );
 }
 
--- react_app/src/components/RegisterForm.js
+++ react_app/src/components/RegisterForm.js
@@ -1,30 +1,30 @@
 import logo from '../logo.svg'
 import './LoginForm.css'
 
-
-export default function LoginForm() {
+export const RegisterForm = () => {
     return (
-        <main className="form-signin">
+         <main className="form-signin">
             <form>
                 <img src={logo} className="App-logo" alt="logo" width="150"/>
-                <h1 className="h3 mb-3 fw-normal text-center">Please sign in</h1>
+                <h1 className="h3 mb-3 fw-normal text-center">Please sign up</h1>
 
                 <div className="form-floating">
                     <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
                     <label htmlFor="floatingInput">Email address</label>
                 </div>
+
                 <div className="form-floating">
                     <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
                     <label htmlFor="floatingPassword">Password</label>
                 </div>
 
-                <div className="checkbox mb-3">
-                    <label>
-                        <input type="checkbox" value="remember-me"/> Remember me
-                    </label>
+                 <div className="form-floating">
+                    <input type="password" className="form-control" id="confirmPassword" placeholder="Confirm Password"/>
+                    <label htmlFor="confirmPassword">Confirm Password</label>
                 </div>
-                <button className="w-100 btn btn-lg btn-primary" type="submit">Login</button>
+
+                <button className="w-100 btn btn-lg btn-info" type="submit">Register</button>
             </form>
         </main>
-    );
+    )
 }
