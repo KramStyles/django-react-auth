81fa0d34bd26489574869094601463c7223d0089
kramstyles <mark.eke@decagon.dev>
2022-05-06 12:38:36 +0100

    added navbar as header

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -3,10 +3,12 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
 import LoginForm from "./components/LoginForm";
 import {RegisterForm} from "./components/RegisterForm";
+import {Navbar} from "./components/Navbar";
 
 function App() {
     return (
         <BrowserRouter>
+            <Navbar/>
             <Routes>
                 <Route path={"/login"} element={<LoginForm />}/>
                 <Route path={"/register"} element={<RegisterForm />}/>
