b2b974854e65f941435c4ea12a12f2a613cb8db4
kramstyles <mark.eke@decagon.dev>
2022-05-09 21:10:21 +0100

    fixed get user on login

--- react_app/src/components/Welcome.js
+++ react_app/src/components/Welcome.js
@@ -2,23 +2,22 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 
 export const Welcome = () => {
+
     const [message, setMessage] = useState('You are not logged in');
-    const [token, setToken] = useState('')
 
     // Assigns access token to a variable if it exists
     useEffect(() => {
         (async () => {
             try {
-                setToken(localStorage.getItem('access_token'));
+
                 const response = await axios.get('http://localhost:5000/api/v1/user/', {
                     withCredentials: true,
-                    headers: {"Authorization": `Bearer ${token}`}
+                    headers: {"Authorization": `Bearer ${localStorage.getItem('access_token')}`}
                 });
 
-                const user = response.data;
+                const user = response.data['data'];
+                setMessage(`${user['first_name']} ${user['last_name']}`);
 
-                console.log('USER');
-                console.log(user);
             } catch (e) {
                 console.log('You are not logged in yet!')
             }
